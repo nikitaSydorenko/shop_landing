01c3ee8eafc9920a2a470b279c3917cb6037fc39
sidorenkonikitka@growthside.app <sidorenkonikitka@growthside.app>
2021-06-15 16:41:02 +0300

    added slider indicators

--- index.js
+++ index.js
@@ -1,24 +1,84 @@
+const buttons = document.querySelectorAll('.button__semanas');
+const tabsMain = document.querySelectorAll('.tab');
 
-
-const buttons = document.querySelectorAll('.button__semanas')
-for(let i = 0; i < buttons.length; i++){
-    buttons[i].classList.remove('active')
-    console.log(buttons[i])
-    buttons[i].addEventListener('click', e => {
-    const p = document.querySelectorAll('.semanas__price')
-        buttons[i].classList.toggle('active')
-            for(let j = 0; j < p.length; j ++){
-                // buttons[j].classList.toggle('active')
-                //
-                //
-                // console.log(p[j])
-            }
-
-
-        let text = p[i].textContent
-        let button_buy = document.querySelector('.button__buy-now');
-        // console.log(button_buy)
-        button_buy.innerText = `Buy now - ${text}`
-    })
+const toggleTab = e => {
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove('active');
+    }
+    const p = e.querySelectorAll('p')
+    let text = p[0].innerHTML
+    let button_buy = document.querySelector('.button__buy-now');
+    button_buy.innerText = `Buy now - ${text.substr(0, 4)}`
+    e.classList.toggle('active');
 }
 
+const toggleTabMain = e => {
+    for (let i = 0; i < tabsMain.length; i++){
+        tabsMain[i].classList.remove('activeMainTab');
+    }
+    e.classList.toggle('activeMainTab');
+
+}
+
+const imageSwitcher = (e) => {
+    const mainImg = document.querySelector('.main__image')
+    const clickedImg = e.querySelector('img').getAttribute('src');
+    mainImg.setAttribute('src', clickedImg);
+    let pupularImg = document.getElementsByClassName('popular');
+    if(clickedImg !== 'images/DSC09153%201.png'){
+
+        pupularImg[0].style.display = "none"
+    }else {
+        pupularImg[0].style.display = "initial"
+    }
+
+}
+const sliderImages = document.querySelectorAll('.slider__image')
+let count = 0;
+let step = 0;
+const imageSwitcherTimer = (e) => {
+    const nextImg = sliderImages[count].querySelector('img').getAttribute('src');
+    if (count === 2 ){
+        count = 0
+    }else {
+        count++
+    }
+    const mainImgTimer = document.querySelector('.main__image')
+    mainImgTimer.setAttribute('src', nextImg)
+    let pupularImg = document.getElementsByClassName('popular');
+    if(nextImg !== 'images/DSC09153%201.png'){
+        pupularImg[0].style.display = "none"
+    }else {
+        pupularImg[0].style.display = "initial"
+    }
+}
+
+
+setInterval(function  (e){
+
+    let currentImg = sliderImages[count]
+    if(step === 0){
+        step++
+        currentImg.classList.add('step1')
+    }else if(step === 1){
+
+        step++
+        currentImg.classList.add('step2')
+    }else if(step === 2){
+        step++
+        currentImg.classList.add('step3')
+    }else if(step === 3) {
+        step++
+        currentImg.classList.add('step4')
+    }else if(step === 4) {
+        currentImg.classList.remove('step1','step2','step3','step4' )
+        step = 0
+        imageSwitcherTimer()
+    }
+} ,1000)
+
+
+function simplePopup() {
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
